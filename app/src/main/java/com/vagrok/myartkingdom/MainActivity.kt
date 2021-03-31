9e31dfafd81422bcb6187cf88ec5bcc88b156eed
KHALED Guesmi <khaled.guesmi@esprit.tn>
2021-03-31 23:03:23 +0100

    main

---
+++ app/src/main/java/com/vagrok/myartkingdom/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vagrok.myartkingdom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //main module
+    }
+}
