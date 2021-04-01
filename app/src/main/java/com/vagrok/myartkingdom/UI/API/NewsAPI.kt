f9888a7c33a93b975a483f48a5f028f91b27a37c
KHALED Guesmi <khaled.guesmi@esprit.tn>
2021-04-01 18:29:02 +0100

    retrofit

--- app/src/main/java/com/vagrok/myartkingdom/UI/API/NewsAPI.kt
+++ app/src/main/java/com/vagrok/myartkingdom/UI/API/NewsAPI.kt
@@ -1,4 +1,36 @@
 package com.vagrok.myartkingdom.UI.API
 
+import com.vagrok.myartkingdom.NewsResponse
+import com.vagrok.myartkingdom.Utility.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsAPI {
+    @GET("/v2/top-headlines")
+    suspend fun getBreakingNews(
+
+        @Query("country")
+        countryCode: String = "tn",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+
+
+    ): Response<NewsResponse>
+
+
+    @GET("/v2/everything")
+    suspend fun searchForNews(
+
+        @Query("q")
+        searchQuery: String ,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+
+
+    ): Response<NewsResponse>
 }
