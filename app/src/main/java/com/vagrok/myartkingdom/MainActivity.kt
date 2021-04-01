9a07ea1d858f2a9ed7120838f8f6820a8863d6ea
KHALED Guesmi <khaled.guesmi@esprit.tn>
2021-04-01 01:22:25 +0100

    suspend function 1 doNetworkCall   suspend fun doNetworkCall():String {         delay(1000L)         return "thread has been delayed by 1 second"     }

--- app/src/main/java/com/vagrok/myartkingdom/MainActivity.kt
+++ app/src/main/java/com/vagrok/myartkingdom/MainActivity.kt
@@ -3,7 +3,9 @@ package com.vagrok.myartkingdom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.Delay
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //Coroutines
         GlobalScope.launch {
-            Log.d(TAG , "this is couroutine opened a new thread : ${Thread.currentThread().name}")
+            Log.d(TAG , "this  coroutine opened a new thread : ${Thread.currentThread().name}")
+            delay(1000L)
+            Log.d(TAG , "After 3 seconds delay this coroutine is back from sleep mode")
         }
+        Log.d(TAG , "another thread opened by the name : ${Thread.currentThread().name}")
+    }
+
+
+
+    //suspension function for coroutines
+
+    suspend fun doNetworkCall():String {
+        delay(1000L)
+        return "thread has been delayed by 1 second"
     }
 }
